6094de262dd0ad362ab060f382ac4739dd90e368
kai0310 <kn0310@outlook.jp>
2022-07-12 01:02:07 +0900

    :bug: fix not set environment-dependent URL

--- config/services.php
+++ config/services.php
@@ -34,7 +34,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://localhost:8000/auth/google/callback',
+        'redirect' => env('APP_URL', 'http://localhost').'/auth/google/callback',
     ],
 
 ];
